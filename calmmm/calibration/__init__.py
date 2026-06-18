aea38c35603ed9c73c6dc82cc1240838a44e3896
knkumar <krankumar@gmail.com>
2026-06-18 07:28:09 -0700

    feat(calibration): add in-model calibration likelihood

--- calmmm/calibration/__init__.py
+++ calmmm/calibration/__init__.py
@@ -1,3 +1,8 @@
 from calmmm.calibration.targets import CalibrationTarget, build_calibration_targets
+from calmmm.calibration.likelihood import add_calibration_likelihood
 
-__all__ = ["CalibrationTarget", "build_calibration_targets"]
+__all__ = [
+    "CalibrationTarget",
+    "build_calibration_targets",
+    "add_calibration_likelihood",
+]
